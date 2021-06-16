d6d873ece74fe6413c437fe8daf267860e625bc3
juan <juan_100@hotmail.com>
2021-06-16 15:21:48 -0500

    coffeApp Env

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 const DBENV = (process.env.NODE_ENV === 'dev') ? 
 			'mongodb://localhost:27017/dbcoffe'
-			:'mongodb://Antik:sjvd9tlPksNOQKnt@cluster0-shard-00-00.cdztu.mongodb.net:27017,cluster0-shard-00-01.cdztu.mongodb.net:27017,cluster0-shard-00-02.cdztu.mongodb.net:27017/dbcoffe?ssl=true&replicaSet=atlas-te1szy-shard-0&authSource=admin&retryWrites=true&w=majority';
+			:process.env.MONGO_URI;
 
 process.env.URL_ENV = DBENV;
 
